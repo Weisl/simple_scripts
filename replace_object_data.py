4c90f02d4647b00af82d3c4ec8463e90fc7013b9
Weisl <weisl12@gmail.com>
2017-03-04 11:04:00 +0100

    add scale all

--- replace_object_data.py
+++ replace_object_data.py
@@ -1,11 +1,17 @@
 import bpy 
 
-data_name = "column_railing.008"
+
 new_data_name = "column_railing.011"
 
+new_mesh_data = "";
+#checks if object exists
+for data in bpy.data.meshes:
+    if data.name == new_data_name:
+        
+        new_mesh_data = data
 
-for obj in bpy.context.selectable_objects:
-    if obj.type == 'MESH':
-        for data in bpy.data.meshes:
-            if data.name == new_data_name:
+        for obj in bpy.context.selected_objects:
+            if obj.type == 'MESH':
                 obj.data = data
+else:
+    ("Data doesn not exist") 
