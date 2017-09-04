6c9803fa43b462c3448ffde021566dbe0f851657
Weisl <weisl12@gmail.com>
2017-09-04 16:32:28 +0200

    added

---
+++ AlignYAxis.py
@@ -0,0 +1,19 @@
+
+""" align objects in y axis"""
+
+import bpy
+
+objs = bpy.context.selected_objects 
+i= 0 
+dist = 0 
+for obj in objs:
+    obj.location.z = 0 
+    obj.location.x = 0 
+    
+    if i == 0:
+        obj.location.y = 0
+        dist += obj.dimensions.y / 2 
+    else: 
+        obj.location.y = dist + obj.dimensions.y / 2
+        dist += obj.dimensions.y 
+    i += 1
