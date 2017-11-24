10af185a094afbc5f9095a02edea7083f5604b41
Matthias <Weisl@users.noreply.github.com>
2017-11-24 15:45:03 +0100

    addedNaming

--- MasterAddon/assets.py
+++ MasterAddon/assets.py
@@ -17,6 +17,114 @@ class colliderEnum:
     def getTuple():
         return tuple(colliderEnum.getList())
 
+class AssetName:
+
+    assetType = {
+        "SM": 1,
+        "SK": 2,
+    }
+    assetLocation = {
+        "pro": 1,
+        "lea": 2,
+        "hub": 4,
+        "par": 8,
+        "tre": 16,
+        "rem": 32,
+        "lia": 64,
+        "cre": 128,
+        "cmn": 256,
+        "tst": 512,
+    }
+
+    assetCategory = {
+        "Int": 1,
+        "Ext": 2,
+        "Nat": 3,
+        "Fur": 4,
+        "Prp": 5,
+        "Chr": 6,
+        "Oth": 7,
+    }
+
+    def __init__(self, name):
+        self.name = name
+
+    def getAssetType(self):
+        assetName = self.name
+        if isNameStructureValid(assetName):
+            obPrefix = assetName[:2]
+            if obPrefix in AssetName.assetType:
+                type = obPrefix
+                return type
+            else:
+                return False
+
+    def getAssetLocation(self):
+        assetName = self.name
+        if isNameStructureValid(assetName):
+            obPrefix = assetName[3:6]
+            if obPrefix in AssetName.assetLocation:
+                location = obPrefix
+                return location
+            else:
+                return False
+
+    def getAssetCategory(self):
+        assetName = self.name
+        if isNameStructureValid(assetName):
+            obPrefix = assetName[6:9]
+            if obPrefix in AssetName.assetCategory:
+                category = obPrefix
+                return category
+            else:
+                return False
+
+    def isNameStructureValid(self):
+        assetName = self.name
+        prog = re.compile(r"^[A-Za-z]{2}_[A-Za-z]{6}_[A-Za-z0-9]+(_[A-Za-z0-9]+)?")
+        match = prog.match(assetName)
+        if match:
+            return True
+        else:
+            return False
+
+    def masterExportPath(self):
+        assetName = self.name
+        print ("Asset Name MasterExportPath " + assetName)
+        AssetName.assetLocation = getAssetLocation(assetName)
+        AssetName.assetCategory = getAssetCategory(assetName)
+
+        if isNameStructureValid(assetName) and AssetName.assetLocation != False and AssetName.assetCategory != False:
+            if bpy.data.is_saved == True:
+
+                basedir = assetPath
+
+                if AssetName.assetLocation != False and AssetName.assetCategory != False:
+
+                    finalPath = os.path.join(basedir, AssetName.assetLocation, "final", AssetName.assetCategory, "")
+                    print (finalPath)
+                    # check for dir "final"
+                    if not os.path.exists(finalPath):
+                        print(os.makedirs(finalPath))
+
+                    return finalPath
+
+        else:  # isNameStructureValid() == False and AssetName.assetLocation == False and AssetName.assetCategory == False
+            return False
+
+    def getObjectCategory(self):
+        assetName = self.name
+        objectCategory = ""
+        if re.search("^SM_[A-Za-z]{6}_", assetName):
+            print("entered Regex")
+            objectCategory = assetName[6:9]
+            print(objectCategory)
+        else:
+            print(assetName + " doesn't fit naming scheme")
+            objectCategory = assetName + " doesn't fit naming scheme"
+        return
+
+
 class Asset:
     def __init__(self, type, name):
         self.name = name
---
+++ MasterAddon/naming.py
@@ -0,0 +1,18 @@
+import bpy, os, re
+
+
+
+assetPath = "D:\\\\01_Dropbox\\Dropbox\\projects\\2017_masterproject\\02_production\\assets"
+unrealAssetPath = ""
+
+
+
+#masterExportPath()
+# print ("getAssetType()" + str(getAssetType()))
+# print("getAssetLocation() " + str(getAssetLocation()))
+# print("getAssetCategory()" + str(getAssetCategory()))
+# print("isNameStructureValid()" + str(isNameStructureValid()))
+# checkAssetType()
+# getObjectCategory()
+
+
