3ba687c8014f952460a1c3e6fb2acd933537d275
Matthias <Weisl@users.noreply.github.com>
2019-05-20 15:09:00 +0200

    Blender 2_80 update

--- Delete_all_not_active_UV_Sets.py
+++ Delete_all_not_active_UV_Sets.py
@@ -6,20 +6,17 @@ selected = bpy.context.selected_objects.copy()
 for obj in selected:
 
     if obj.type == 'MESH':
-        bpy.context.scene.objects.active = obj
-        uvActive = obj.data.uv_textures.active
-        texSetList = list(obj.data.uv_textures)
+        bpy.context.view_layer.objects.active = obj
+        uvActive = obj.data.uv_layers.active
+        texSetList = list(obj.data.uv_layers)
 
         for tex in texSetList:
             if tex != uvActive:
                 try:
-                    obj.data.uv_textures.remove(tex)
+                    obj.data.uv_layers.remove(tex)
                     print ("Deleted %s on object %s" % (str(tex), obj.name))
 
                 except Exception:
                     print ("couldn't delete textuteset %s on object %s" % (str(tex), obj.name))
             else:
                 print ("LOL")
-
-
-
