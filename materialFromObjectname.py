4f68d08e03ee395331f6d753a84d8aefebfaffe1
Matthias <Weisl@users.noreply.github.com>
2019-10-25 22:00:59 +0200

    material fro object

--- materialFromObjectname.py
+++ materialFromObjectname.py
@@ -2,6 +2,7 @@
 import bpy
 import re
 
+
 def makeMaterial(name, diffuse):
     b_mat_exists = False
     for mat in bpy.data.materials:
@@ -13,32 +14,34 @@ def makeMaterial(name, diffuse):
         mat.diffuse_color = diffuse
     return mat
 
+
 def setMaterial(ob, mat):
     me = ob.data
     me.materials.append(mat)
 
+
 for obj in bpy.context.selected_objects:
     if obj.type == 'MESH':
-        objectname = obj.name 
-        objectname = re.sub('^[A-Z][A-Z]_', '', objectname) #regex Cut Prefix Away
+        objectname = obj.name
+        objectname = re.sub('^[A-Z][A-Z]_', '', objectname)  # regex Cut Prefix Away
 
         matName = "MI_" + objectname
-        #matName.replace('_LP', '_MAT')
-        
+        # matName.replace('_LP', '_MAT')
+
         if matName in bpy.data.materials:
             for mat in bpy.data.materials:
                 if mat.name == matName:
                     material = mat
         else:
-            material = makeMaterial(matName, (0.8,0.8,0.8))
-       
-        bpy.context.scene.objects.active = obj
-            
-        for i in range(0,len(obj.material_slots)):
+            material = makeMaterial(matName, (0.8, 0.8, 0.8, 1))
+
+        bpy.context.view_layer.objects.active = obj
+
+        for i in range(0, len(obj.material_slots)):
             obj.active_material_index = 1
             bpy.ops.object.material_slot_remove()
-          
+
         setMaterial(obj, material)
-    
-        
+
+
 
