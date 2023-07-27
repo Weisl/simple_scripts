4c1158f6af2f3257ba3a09cb5aac6dc935825235
Matthias <Weisl@users.noreply.github.com>
2023-07-27 17:43:39 +0300

    Update uv_convert_cornerAttribute_to_UV.py

--- uv/uv_convert_cornerAttribute_to_UV.py
+++ uv/uv_convert_cornerAttribute_to_UV.py
@@ -3,10 +3,15 @@ Convert Face Corner Attributes to UV maps. Useful for Geometry Nodes.
 based on: https://blender.stackexchange.com/questions/250867/overwrite-original-geometry-uvmap-with-geometry-nodes-uv-map'''
 
 import bpy
-
-UVAttributeName = 'customUV'
-for obj in bpy.context.selected_objects.copy():
+selected_objects = bpy.context.selected_objects.copy()
+active_object = bpy.context.active_object
+for obj in selected_objects:
+    for ob in selected_objects:
+        ob.select_set(False)
     bpy.context.view_layer.objects.active = obj
-    a = bpy.context.object.data.attributes
-    a.active = a[UVAttributeName]
+    obj.select_set(True)
+
+    bpy.context.view_layer.objects.active = obj
+    a = obj.data.attributes
+    a.active = a['customUV']
     bpy.ops.geometry.attribute_convert(mode='GENERIC', domain='CORNER', data_type='FLOAT2')
